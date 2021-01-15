ed3fe73e44472fe6c7c1a7fadebc7e3053156a37
unknown <trcasper90@gmail.com>
2021-01-15 13:17:29 -0700

    setup and header

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <div className='Header'>
+        <div className='Title'>Who's My Representative?</div>
+      </div>
+      <div className='Button-container'>
+        <button className='Submit-button'>SUBMIT</button>
+      </div>
     </div>
   );
 }
