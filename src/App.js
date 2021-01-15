da2c9803749e1b4601499feb40ecf50ec59944b2
unknown <trcasper90@gmail.com>
2021-01-15 14:21:36 -0700

    working on buttons

--- src/App.js
+++ src/App.js
@@ -3,11 +3,18 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className='App-container'>
       <div className='Header'>
         <div className='Title'>Who's My Representative?</div>
       </div>
+      <div className='Selector-container'>
+        <button className='Select-rep'>Rep/Sen</button>
+
+
+      </div>
       <div className='Button-container'>
         <button className='Submit-button'>SUBMIT</button>
+      </div>
       </div>
     </div>
   );
